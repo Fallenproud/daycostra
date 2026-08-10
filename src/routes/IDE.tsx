ede66b9fdffa55a51e2e6f36aa4c0c0bd252b771
Fallenproud <mr.lyder@hotmail.com>
2026-08-10 05:11:09 +0200

    feat: add uppercase IDE compatibility route

---
+++ src/routes/IDE.tsx
@@ -0,0 +1,9 @@
+import { Navigate, createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/IDE")({
+  component: LegacyIdeRedirect,
+});
+
+function LegacyIdeRedirect() {
+  return <Navigate to="/ide" replace />;
+}
